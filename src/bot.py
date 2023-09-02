318d52d138e30adba9fea4cc1bf155ce758483e5
Kazuki Tobita <kazukiti201@gmail.com>
2023-09-02 14:39:41 +0900

    Add reminder

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,11 @@
 import os
 import random
 import re
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.date import DateTrigger
 from dotenv import load_dotenv
 from slack_bolt import App
 
@@ -14,6 +15,7 @@ load_dotenv()
 bot_user_oauth_token = os.environ["BOT_USER_OAUTH_TOKEN"]
 signing_secret = os.environ["SIGNING_SECRET"]
 app = App(token=bot_user_oauth_token, signing_secret=signing_secret)
+scheduler = BackgroundScheduler()
 
 members = [
     {
@@ -40,12 +42,16 @@ def handle_app_mentions(body, say, logger) -> None:
     logger.info(body)
 
     sender_message = re.sub(r"<@\w+>", "", body["event"]["text"]).strip()
+    sender_command = sender_message.split()[0]
 
-    if sender_message == "usage":
+    if sender_command == "usage":
         receiver_message = reply_usage()
-    elif sender_message == "minutes":
+    elif sender_command == "mtg":
+        date = sender_message.split()[1]
+        receiver_message = set_mtg_date(date=date)
+    elif sender_command == "minutes":
         receiver_message = reply_minutes_pic()
-    elif sender_message == "presen":
+    elif sender_command == "presen":
         receiver_message = reply_presentation_order()
     else:
         receiver_message = reply_random_message()
@@ -55,6 +61,7 @@ def handle_app_mentions(body, say, logger) -> None:
 
 def reply_usage() -> str:
     usage_messages = [
+        "@hisho mtg: 今月の月次報告会の日時を設定します。"
         "@hisho minutes: 議事録の各担当者を決めます。",
         "@hisho presen: 発表順を決めます。",
         "@hisho usage: このメッセージを表示します。",
@@ -62,6 +69,35 @@ def reply_usage() -> str:
     ]
     message =  "秘書の井ノ上たきなです。\n私にできる仕事をまとめました。\n\n" + "\n".join(usage_messages)
     return message
+
+
+def remind_mtg_candidate_date():
+    # channel_id = "C01U1QN5M5E"
+    channel_id = "C05QN0U6U3U"
+    app.client.chat_postMessage(
+        channel=channel_id,
+        text="<!channel> 今月の月次報告会の候補日を提出してください。"
+    )
+
+
+def remind_mtg_date():
+    # channel_id = "C01U1QN5M5E"
+    channel_id = "C05QN0U6U3U"
+    app.client.chat_postMessage(
+        channel=channel_id,
+        text="<!channel> 明日の21時〜月次報告会です。"
+    )
+
+
+def set_mtg_date(date: str) -> str:
+    mtg_date = datetime.strptime(date, "%Y/%m/%d")
+    reminder_time = mtg_date - timedelta(days=1)
+    reminder_time = reminder_time.replace(hour=9, minute=0, second=0, microsecond=0)
+    scheduler.add_job(
+        remind_mtg_date,
+        trigger=DateTrigger(reminder_time),
+    )
+    return "月次報告会の前日の朝9時にリマインドを設定しました。"
 
 
 def reply_minutes_pic() -> str:
@@ -95,22 +131,12 @@ def reply_random_message() -> str:
     return message
 
 
-def remind_mtg_candidate_date():
-    # channel_id = "C01U1QN5M5E"
-    channel_id = "C05QN0U6U3U"
-    app.client.chat_postMessage(
-        channel=channel_id,
-        text="<!channel> 今月の月次報告会の候補日を提出してください。"
-    )
-
-scheduler = BackgroundScheduler()
 scheduler.add_job(
     remind_mtg_candidate_date,
     # trigger=CronTrigger(minute="30"),
     trigger=CronTrigger(day="15"),
 )
 scheduler.start()
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
